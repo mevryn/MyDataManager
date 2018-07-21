d3f366b2ceb9ac72a0550e42d832150d03ab6789
mevryn <gelnir000@gmail.com>
2018-07-22 01:06:51 +0200

    Basic logic implemmented

--- src/Data/Manager.java
+++ src/Data/Manager.java
@@ -1,31 +1,47 @@
 package Data;
 
-import Command.CommandProvider;
 import Command.Status;
-import Data.DataProvider;
 import Element.BoardGame;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
-import Command.Status;
+
 public class Manager {
-    private CommandProvider commandProvider;
-    private DataProvider dataProvider;
     private Status status;
-    public Manager(CommandProvider commandProvider,DataProvider dataProvider ){
-        this.commandProvider = commandProvider;
-        this.dataProvider = dataProvider;
+    List<BoardGame> boardGames= new ArrayList<BoardGame>();
+    public Manager( ){
+        status = Status.ACTIVE;
     }
     public void getCommand()
     {
         Scanner input = new Scanner(System.in);
         while(status==Status.ACTIVE){
-            System.out.println("Type 'add' to add new Boardgame");
-            if (input.nextLine() =="add"){
-                String name = input.nextLine();
-                int numberOfPlayers = input.nextInt();
-                BoardGame game = new BoardGame(name,numberOfPlayers);
-                dataProvider.addToList(game);
+            System.out.println("Insert command : \n add to add new Game, \n exit to Quit, \n show to show List of Games");
+            String command = input.nextLine();
+            String name;
+            int numberOfPlayers;
+            if(command.equals("add") || command.equals("Add")) {
+                System.out.println("Insert name of board game");
+                name = input.nextLine();
+                System.out.println("Insert maximum players that can play the game at one time");
+                numberOfPlayers = input.nextInt();
+                boardGames.add(new BoardGame(name, numberOfPlayers));
+                input.nextLine();
+            }else if (command.equals("exit") || command.equals("Exit")) {
+                status = Status.STOP;
+            }else if(command.equals("show") || command.equals("Show")) {
+                this.printGames();
+            }
+            else {
+                System.out.println("Wrong Command");
             }
         }
+        return;
+    }
+    public void printGames() {
+        for (BoardGame element : this.boardGames)
+            System.out.println(element.getName());
     }
 }
+
--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,8 @@
-import Command.CommandProvider;
-import Data.DataProvider;
 import Data.Manager;
-import Element.BoardGame;
-
-import java.awt.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
-        List<BoardGame> boardGames= new ArrayList<BoardGame>();
-        Scanner input = new Scanner(System.in);
-        String name;
-        int numberOfPlayers;
-        while(boardGames.size()<3) {
-             name =  input.nextLine();
-             numberOfPlayers = input.nextInt();
-            boardGames.add(new BoardGame(name, numberOfPlayers));
-        }
-        for(BoardGame element:boardGames)
-            System.out.println(element.getName());
+    public static void main(String[] args) {
+        Manager manager = new Manager();
+        manager.getCommand();
     }
 }
